33f6db4b41d6ffab815742a71977a29dd45e786a
tjugosex <mikael.skaremyr.te1@hotmail.com>
2024-03-30 08:05:44 +0100

    Added Shuffler method

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -7,6 +7,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +21,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button b = findViewById(R.id.myShflBtn);
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("==>","Button clicked");
+                TextView textViewToShuffle = (TextView) findViewById(R.id.textToShfl);
+                EditText myNewEditText = (EditText) findViewById(R.id.myNewEdt);
+                CharSequence textFromEditText = myNewEditText.getText();
+                textViewToShuffle.setText(textFromEditText);
             }
         });
     }
+public String shuffler (String textToShuffle){
+    List<Character> charactersToShuffle = new ArrayList<>();
 
+    for (char i : textToShuffle.toCharArray()) {
+        charactersToShuffle.add(i);
+    }
+
+    Collections.shuffle(charactersToShuffle);
+
+    StringBuilder backToString = new StringBuilder();
+    for (char i : charactersToShuffle) {
+        backToString.append(i);
+    }
+
+    return backToString.toString();
+}
 
 }
